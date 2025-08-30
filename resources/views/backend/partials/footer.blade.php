6db47828ec42f618c71c971a80350235fe29ba50
reznlar-02 <nuranisarezanti03@gmail.com>
2025-08-30 20:15:20 +0700

    Ubah nama aplikasi di footer dari UNPAM Education menjadi Reznlar Education

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <p>2024 &copy; UNPAM Education</p>
+                <p>2024 &copy; Reznlar Education</p>
             </div>
         </div>
     </div>
