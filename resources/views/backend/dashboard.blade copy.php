6954b085ba998a8ab1f7940e8753702ebcbd8065
reznlar-02 <nuranisarezanti03@gmail.com>
2025-08-30 20:16:12 +0700

    Ubah judul aplikasi dari Mazer Admin Dashboard menjadi Reznlar Education

--- resources/views/backend/dashboard.blade copy.php
+++ resources/views/backend/dashboard.blade copy.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Mazer Admin Dashboard</title>
+    <title>Dashboard - Reznlar Education</title>
 
     <link rel="shortcut icon" href="{{ asset('backend/assets/compiled/svg/favicon.svg') }}" type="image/x-icon">
     <link rel="shortcut icon"
